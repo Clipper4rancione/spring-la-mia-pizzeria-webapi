d9f018e06be81385869c53b2187dae1589174a9b
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-30 16:32:49 +0200

    commit class and app properties

---
+++ src/main/java/com/example/springlamiapizzeriacrud/model/Pizza.java
@@ -0,0 +1,38 @@
+package com.example.springlamiapizzeriacrud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Lob
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
