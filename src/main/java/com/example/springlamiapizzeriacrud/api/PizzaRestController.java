c4c821932000bcd952345c9ec01e38c7a80f1c26
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-04-11 15:50:22 +0200

    [ADD] Created Rest Controller

---
+++ src/main/java/com/example/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -0,0 +1,23 @@
+package com.example.springlamiapizzeriacrud.api;
+
+import com.example.springlamiapizzeriacrud.controller.PizzaController;
+import com.example.springlamiapizzeriacrud.model.Pizza;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pizza")
+public class PizzaRestController {
+
+    @Autowired
+    private PizzaController pizzaController;
+
+    @GetMapping
+    public List<Pizza> list() {
+        return pizzaController.index();
+    }
+}
